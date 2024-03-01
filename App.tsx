22700c9d65e5f2c46ed37c09bd2556009f8ef471
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2024-03-01 15:28:21 -0300

    feat: bird physics

--- App.tsx
+++ App.tsx
@@ -4,10 +4,18 @@ import {
     withRepeat,
     withSequence,
     useSharedValue,
+    useFrameCallback,
 } from "react-native-reanimated";
+import {
+    Gesture,
+    GestureDetector,
+    GestureHandlerRootView,
+} from "react-native-gesture-handler";
 import { useEffect } from "react";
 import { useWindowDimensions } from "react-native";
 import { Canvas, useImage, Image } from "@shopify/react-native-skia";
+
+const GRAVITY = 500;
 
 const App = () => {
     const { width, height } = useWindowDimensions();
@@ -20,59 +28,83 @@ const App = () => {
 
     const x = useSharedValue(width);
 
+    const birdY = useSharedValue(height / 2);
+    const birdYVelocity = useSharedValue(100);
+
+    useFrameCallback(({ timeSincePreviousFrame: dt }) => {
+        if (!dt) return;
+
+        birdY.value = birdY.value + (birdYVelocity.value * dt) / 1000;
+        birdYVelocity.value = birdYVelocity.value + (GRAVITY * dt) / 1000;
+    });
+
     useEffect(() => {
         x.value = withRepeat(
             withSequence(
                 withTiming(-150, { duration: 3000, easing: Easing.linear }),
-                withTiming(width, { duration: 0 }),
+                withTiming(width, { duration: 0 })
             ),
             -1
         );
     }, []);
 
+    const gesture = Gesture.Tap().onStart(() => {
+        birdYVelocity.value = -300;
+    });
+
     const pipeOffset = 0;
+
     return (
-        <Canvas style={{ width, height }}>
-            {/* Background */}
-            <Image image={bg} fit={"cover"} width={width} height={height} />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <GestureDetector gesture={gesture}>
+                <Canvas style={{ width, height }}>
+                    {/* Background */}
+                    <Image
+                        image={bg}
+                        fit={"cover"}
+                        width={width}
+                        height={height}
+                    />
 
-            {/* Cano superior */}
-            <Image
-                image={pipeTop}
-                x={x}
-                y={pipeOffset - 320}
-                width={103}
-                height={640}
-            />
+                    {/* Cano superior */}
+                    <Image
+                        image={pipeTop}
+                        x={x}
+                        y={pipeOffset - 320}
+                        width={103}
+                        height={640}
+                    />
 
-            {/* Cano inferior */}
-            <Image
-                image={pipeBottom}
-                x={x}
-                y={height - 320 - pipeOffset}
-                width={103}
-                height={640}
-            />
+                    {/* Cano inferior */}
+                    <Image
+                        image={pipeBottom}
+                        x={x}
+                        y={height - 320 - pipeOffset}
+                        width={103}
+                        height={640}
+                    />
 
-            {/* base */}
-            <Image
-                image={base}
-                width={width}
-                height={150}
-                y={height - 75}
-                x={0}
-                fit={"cover"}
-            />
+                    {/* base */}
+                    <Image
+                        image={base}
+                        width={width}
+                        height={150}
+                        y={height - 75}
+                        x={0}
+                        fit={"cover"}
+                    />
 
-            {/* Pássaro */}
-            <Image
-                image={bird}
-                x={width / 4}
-                y={height / 2}
-                width={64}
-                height={48}
-            />
-        </Canvas>
+                    {/* Pássaro */}
+                    <Image
+                        image={bird}
+                        x={width / 4}
+                        y={birdY}
+                        width={64}
+                        height={48}
+                    />
+                </Canvas>
+            </GestureDetector>
+        </GestureHandlerRootView>
     );
 };
 
